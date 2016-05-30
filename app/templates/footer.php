9d7be1258ee17e5063e4ab28a45725c993c93b2e
Hyojin Jung <hyojinjenny@gmail.com>
2016-05-31 08:42:50 +1200

    Update html php template

---
+++ app/templates/footer.php
@@ -0,0 +1,7 @@
+	 	<footer>
+	 		<p>copyright &copy;	<?php echo date('jS M Y') ?></p> 	
+	 	</footer>
+
+	</body>
+	
+</html>
---
+++ app/templates/header.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Home </title>
+		<meta name="description" content="Between 150 and 160 characters">
+		<link rel="stylesheet" href="css/styles.css">
+
+	</head>
+	<body>
+
+	 <!-- Set the New Zealand time -->
+	<?php date_default_timezone_set('Pacific/Auckland'); ?>
+
+	<nav>
+		<ul>
+			<li><a href="index.php?page=home">Home</a></li>
+			<li><a href="index.php?page=products">Products</a></li>
+			<li><a href="index.php?page=contact">Contact</a></li>
+			<li><a href="index.php?page=register">Register</a></li>
+			<li><a href="index.php?page=login">Log in</a></li>
+		</ul>
+	</nav>
